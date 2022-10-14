dc7ea06f3dc3ef411dc14fad769747bbcc637cdb
Cassie Ryu <793399678@qq.com>
2022-10-14 18:39:04 +0800

    Connect redux toolkit state

--- 08-Counter-Redux/src/store/index.js
+++ 08-Counter-Redux/src/store/index.js
@@ -1,38 +1,74 @@
-import { legacy_createStore } from 'redux';
+import { legacy_createStore, configureStore } from 'redux';
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = { counter: 0, showCounter: true };
-const counterReducer = (state = initialState, action) => {
-    if (action.type === 'increment') {
-        return {
-            counter: state.counter + 1,
-            showCounter: state.showCounter,
-        };
-    }
 
-    if (action.type === 'increase') {
-        return {
-            counter: state.counter + action.amount,
-            showCounter: state.showCounter,
-        };
-    }
+/**
+ * Use createSlice method instead of createReducer
+ */
 
-    if (action.type === 'decrement') {
-        return {
-            counter: state.counter - 1,
-            showCounter: state.showCounter,
-        };
-    }
+const counterSlice = createSlice({
+    name: 'counter', // state name
+    initialState,
+    reducers: {
+        increment(state) {
+            // NOT use this in createReducer, use reference instead!
+            //because manipulate with state directly is highly not recommend, easily to cause bug
+            state.counter++;
+            // ABLE to use here because createSlice has auto transfer such code to use reference
+        },
+        decrement(state) {
+            state.counter--;
+        },
+        increase(state) {
+            state.counter = state.counter + action.amount;
+        },
+    },
+});
 
-    if (action.type === 'toggle') {
-        return {
-            counter: state.counter,
-            showCounter: !state.showCounter,
-        };
-    }
+const store = configureStore({
+    reducer: counterSlice.reducer,
+    // reducer: { counter: counterSlice.reducer }, // used for multiple reducers
+});
 
-    return state;
-};
+// /**
+//  * use createReducer method
+//  * @param {*} state
+//  * @param {pass from calling func} action
+//  * @returns
+//  */
+// const counterReducer = (state = initialState, action) => {
+//     if (action.type === 'increment') {
+//         return {
+//             counter: state.counter + 1,
+//             showCounter: state.showCounter,
+//         };
+//     }
 
-const store = legacy_createStore(counterReducer);
+//     if (action.type === 'increase') {
+//         return {
+//             counter: state.counter + action.amount,
+//             showCounter: state.showCounter,
+//         };
+//     }
+
+//     if (action.type === 'decrement') {
+//         return {
+//             counter: state.counter - 1,
+//             showCounter: state.showCounter,
+//         };
+//     }
+
+//     if (action.type === 'toggle') {
+//         return {
+//             counter: state.counter,
+//             showCounter: !state.showCounter,
+//         };
+//     }
+
+//     return state;
+// };
+
+// const store = legacy_createStore(counterReducer);
 
 export default store;
